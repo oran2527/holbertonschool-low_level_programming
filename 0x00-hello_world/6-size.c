aedb93a88a0602b56d83f874dc07c6b3ba6e2721
oran2527 <orlago250183@gmail.com>
2019-09-18 21:00:13 +0000

    first

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main()
+{
+	   printf("Size of char: %d"     ,sizeof(char*));
+           printf("Size of int: %d"      ,sizeof(int*));
+	   printf("Size of long int: %d" ,sizeof(long int*));
+	   printf("Size of long long int: %d" ,sizeof(double*));
+	   printf("Size of a float: %d" ,sizeof(float*));
+	   return (0);
+}
+
+
