c9f974afb81a96d90e881dc0bbb6633c69527642
oran2527 <orlago250183@gmail.com>
2019-09-19 21:03:49 +0000

    fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,14 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
  * Description alphabet
  * Return: Always 0 (Success)
- */
+**/
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+char n;
 for (n = 'a'; n <= 'z'; n++)
 {
 putchar(n);
 }
-putchar('\n');
-return (0);
-}
+p
