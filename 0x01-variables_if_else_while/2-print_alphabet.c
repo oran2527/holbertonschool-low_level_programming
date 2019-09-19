3bd9de81d0595e99c8aebc31fa287cecda11456d
oran2527 <orlago250183@gmail.com>
2019-09-19 20:28:33 +0000

    first

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	srand(time(0));
+	ch = rand() - RAND_MAX / 2;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
