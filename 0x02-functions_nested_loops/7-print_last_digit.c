b7efda4931273021877deb704252a9d68dbe17bb
oran2527 <orlago250183@gmail.com>
2019-09-24 13:57:42 +0000

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,9 @@ int print_last_digit(int n)
 {
 if (n < 0)
 {
-n = (n * (-1)) - (((n * (-1)) / 10) * 10);
+n = n * (-1);
 }
-if (n > 0)
-{
 n = n - ((n / 10) * 10);
-}
-if (n == 0)
-{
-n = n;
-}
 _putchar(n + '0');
 return (n);
 return (0);
