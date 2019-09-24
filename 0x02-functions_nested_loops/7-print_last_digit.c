2d1e15b6fde20e839f50f7ce1e768ad3a4873a24
oran2527 <orlago250183@gmail.com>
2019-09-24 13:42:22 +0000

    first

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints
+ * @n: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ **/
+int print_last_digit(int n)
+{
+if (n < 0)
+{
+n = (n * (-1)) - (((n * (-1)) / 10) * 10);
+}
+if (n > 0)
+{
+n = n - ((n / 10) * 10);
+}
+_putchar(n + '0');
+return (n);
+}
