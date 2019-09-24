0d6aaeaae9249e09f7808b63179581b5448935b8
oran2527 <orlago250183@gmail.com>
2019-09-24 15:57:00 +0000

    first

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * times_table - prints
+ * @n,c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ **/
+void times_table(void)
+{
+int n;
+int c;
+
+for (n = 0; n <= 9; n++)
+{
+for (c = 0; c <= 9; c++)
+{
+if ((n * c) == 0 && c == 0)
+{
+_putchar((n * c) + 48);
+}
+if ((n * c) <= 9 && c != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((n * c) + 48);
+}
+if ((n * c) >= 10 && c != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(((n * c) / 10) + 48);
+_putchar(((n * c) % 10) + 48);
+}
+}
+_putchar ('\n');
+}
+}
