980e3709d74a960f24936c9395293b7afafd1d45
oran2527 <orlago250183@gmail.com>
2019-09-24 00:25:58 +0000

    first

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * prototypes
+ */
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+#endif /* _HOLBERTON_H_ */
