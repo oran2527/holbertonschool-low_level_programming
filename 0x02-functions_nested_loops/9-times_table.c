d0fc86c43a32850cedf57e747455b2b728ed7253
oran2527 <orlago250183@gmail.com>
2019-09-24 17:40:52 +0000

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,23 +14,25 @@ for (n = 0; n <= 9; n++)
 {
 for (c = 0; c <= 9; c++)
 {
-if ((n * c) == 0 && c == 0)
+if (c == 0)
 {
-_putchar((n * c) + 48);
+_putchar('0');
 }
+else
+{
 _putchar(',');
 _putchar(' ');
-if ((n * c) <= 9 && c != 0)
+if ((n * c) <= 9)
 {
 _putchar(' ');
-_putchar((n * c) + 48);
 }
-if ((n * c) >= 10 && c != 0)
+else
 {
-_putchar(((n * c) / 10) + 48);
-_putchar(((n * c) % 10) + 48);
+_putchar((n * c) / 10 + 48);
+}
+_putchar((n * c) % 10 + 48);
 }
 }
-_putchar ('\n');
+_putchar('\n');
 }
 }
