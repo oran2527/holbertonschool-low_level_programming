c18b013b7395ebb74884d2bdb0daebdc45ec14dd
oran2527 <orlago250183@gmail.com>
2019-09-26 20:10:34 +0000

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - check the code for Holberton School students.
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
 {
 	int n;
-	
+
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 != 0 && n % 5 != 0)
@@ -23,10 +23,9 @@ int main(void)
 			if (n % 5 == 0)
 				printf("Buzz");
 		}
-		
+
 		if (n != 100)
 			putchar(' ');
-			
 	}
 	putchar('\n');
 	return (0);
