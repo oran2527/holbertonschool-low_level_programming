0fcea52affd0882d93193921f73b88fa98ccb7f1
oran2527 <orlago250183@gmail.com>
2019-09-26 15:58:49 +0000

    first

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -12,5 +12,9 @@ int _isupper(char);
 int _isdigit(char);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
 
 #endif /* _HOLBERTON_H_ */
