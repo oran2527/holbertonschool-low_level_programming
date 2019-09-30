c4edb7ee1da079b91393d5c7de633462915363d5
oran2527 <orlago250183@gmail.com>
2019-09-30 19:26:57 +0000

    first

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,17 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * prototypes
+ */
+
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+
+
+
+#endif /* _HOLBERTON_H_ */
