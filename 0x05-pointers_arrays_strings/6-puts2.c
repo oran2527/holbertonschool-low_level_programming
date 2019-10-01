50203c2157c8e800fcb5b594dd1b13378316573c
oran2527 <orlago250183@gmail.com>
2019-10-01 20:00:14 +0000

    fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,10 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while ((*(str + i)) != '\0')
 	{
-		if (str[i] % 2 == 0)
-			_putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar((*(str + i)));
 		++i;
 	}
 	_putchar('\n');
