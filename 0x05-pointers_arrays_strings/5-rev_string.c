09b28af388b8d4e8f9608b039723d3b59e9ae08b
oran2527 <orlago250183@gmail.com>
2019-10-01 01:03:25 +0000

    first

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+#include "holberton.h"
+
+/**
+ * rev_string - prints
+ * @s: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ **/
+void rev_string(char *s)
+{
+	char *strputs;
+	int i;
+	int c;
+	int j;
+	char *cadena [sizeof(s)];
+
+	strputs = s;
+	i = 0;
+	while (strputs[i] != '\0')
+	{
+		++i;
+	}
+	j = 0;
+	for (c = i; c >= 0; c--)
+	{
+		cadena[j] = strputs[c];
+		++j;
+	}
+	s = cadena;
+}
