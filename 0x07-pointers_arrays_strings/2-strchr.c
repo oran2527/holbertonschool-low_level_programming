94ee0043a90457ae1470837fc0cb2ac6b0fc5c30
oran2527 <orlago250183@gmail.com>
2019-10-08 00:10:50 +0000

    first

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * _strchr - prints
@@ -10,27 +10,16 @@
  **/
 char *_strchr(char *s, char c)
 {
-	int length_s = 0;
-	int i = 0;
-	int k = 0;
-	char *final = NULL;
+	char *pointer;
 
-	while (*(s + length_s) != '\0')
-		++length_s;
-	if (length_s == 0)
+	if (s == NULL)
+		return (NULL);
+	pointer = s;
+	while (*pointer != '\0')
 	{
-		return (final);
+		if (*pointer == c)
+			return (pointer);
+		pointer++;
 	}
-	else
-	{
-		while ((*(s + i) != c) && (i <= length_s - 1))
-		{
-			++i;			
-		}
-		for ( ; i <= length_s - 1; i++, k++)
-		{
-			*(final + k) = *(s + i + 1);
-		}
-		return (final);
-	}
+	return (pointer);
 }
