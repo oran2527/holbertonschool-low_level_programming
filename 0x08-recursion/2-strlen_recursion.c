b513ce0b386d23150b88892da14c77bf3dc3f1c8
oran2527 <orlago250183@gmail.com>
2019-10-09 18:47:12 +0000

    fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -14,7 +14,7 @@ int _strlen_recursion(char *s)
 	if (*s != '\0')
 	{
 		increment++;
-		length += increment + _strlen_recursion(s + 1);	
+		length += increment + _strlen_recursion(s + 1);
 		return (length);
 	}
 	else
