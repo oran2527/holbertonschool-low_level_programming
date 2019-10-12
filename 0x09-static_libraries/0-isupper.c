8d66f5e8ed3a2fa7f1fe2d697c602a71dd8dae07
oran2527 <orlago250183@gmail.com>
2019-10-12 23:27:06 +0000

    first

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,11 +1,13 @@
 #include "holberton.h"
 /**
- * _isupper - prints
- * @c: The character to print
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately.
- **/
+ *_isupper - whatever
+ *@c: input value to check
+ *Return: response
+ */
 int _isupper(int c)
 {
-
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
