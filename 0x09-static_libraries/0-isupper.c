fae9b7e47998e4e6cb3c0dac4ef1d17c676f3ad7
oran2527 <orlago250183@gmail.com>
2019-10-12 21:42:39 +0000

    first

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _isupper - prints
+ * @c: The character to print
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ **/
+int _isupper(int c)
+{
+
+}
