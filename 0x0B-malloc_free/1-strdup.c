46281851965d58832f6f1e06a26fb5535e239b5d
oran2527 <orlago250183@gmail.com>
2019-10-15 18:59:40 +0000

    first

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,27 +1,37 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * create_array - create array with malloc
- * @size: Amount of elements
- * @c: Character to print
+ * _strdup - duplicate an array
+ * @str: String
  * Return:  pointer1
  * On error, NULL is returned
  **/
 char *_strdup(char *str)
 {
 	char *pointer1;
-	int i;
+	unsigned int i, j = 0;
 
 	if (str == NULL)
 	{
 		pointer1 = NULL;
+		return (pointer1);
 	}
 	else
 	{
-		for (i = 0; i != '\0'; i++)
-		pointer1 = (char *)malloc(i * sizeof(*pointer1));
-		
+		while (*(str + j) != '\0')
+			j++;
+		pointer1 = (char *)malloc((j + 1) * sizeof(*pointer1));
+		if (pointer1 != NULL)
+		{
+			for (i = 0; i < j; i++)
+				*(pointer1 + i) = *(str + i);
+			*(pointer1 + (i + 1)) = '\0';
+		}
+		else
+		{
+			pointer1 = NULL;
+			return (pointer1);
+		}
 	}
 	return (pointer1);
 }
-
