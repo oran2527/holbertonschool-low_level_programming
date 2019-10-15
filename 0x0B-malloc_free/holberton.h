a3675a05114a58fe94d37ab83ed2d754cc2e244e
oran2525 <raumedina010101@gmail.com>
2019-10-15 12:22:08 +0000

    fix

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * prototypes
+ */
+#include <stdio.h>
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif /* _HOLBERTON_H_ */
