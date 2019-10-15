c509bd74f2234751e5fc94ec2b2eb6cead575a07
oran2527 <orlago250183@gmail.com>
2019-10-15 18:16:42 +0000

    fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,24 +10,26 @@
 char *create_array(unsigned int size, char c)
 {
 	char *pointer1;
-	int i;
+	unsigned int i;
 
 	if (size == 0)
 	{
 		pointer1 = NULL;
+		return (pointer1);
 	}
 	else
 	{
 		pointer1 = (char *)malloc(size * sizeof(*pointer1));
 		if (pointer1 != NULL)
 		{
-			for (i = 0; i < 98; i++)
+			for (i = 0; i < size; i++)
 				*(pointer1 + i) = c;
 			*(pointer1 + (i + 1)) = '\0';
 		}
 		else
 		{
 			pointer1 = NULL;
+			return (pointer1);
 		}
 	}
 	return (pointer1);
