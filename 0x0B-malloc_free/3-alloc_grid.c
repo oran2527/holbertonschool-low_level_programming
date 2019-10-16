c822c209a96e470f3bc40a1d550ff0876a33ddb6
oran2525 <raumedina010101@gmail.com>
2019-10-16 03:26:08 +0000

    fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdlib.h>
 /**
  * alloc_grid - pointer bidimensional
  * @width: width
@@ -9,19 +8,25 @@
 int **alloc_grid(int width, int height)
 {
 	int **rows, i, j;
-	
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	rows = (int **)malloc(height * sizeof(int *));
 	if (rows == NULL)
+	{
+		free(rows);
 		return (NULL);
+	}
 	if (rows != NULL)
 	{
 		for (i = 0; i < height; i++)
 		{
 			rows[i] = (int *)malloc(width * sizeof(int));
 			if (rows[i] == NULL)
+			{
+				free(rows[i]);
 				return (NULL);
+			}
 		}
 		for (i = 0; i < height; i++)
 		{
@@ -30,7 +35,6 @@ int **alloc_grid(int width, int height)
 				*(*(rows + i) + j) = 0;
 			}
 		}
-		return (rows);
 	}
 	return (rows);
 }
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -6,7 +6,7 @@
  * Typedefs
  * prototypes
  */
-#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
