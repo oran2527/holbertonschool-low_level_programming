34493539fb5e8e5c6894b0854a2dbb9fdf33b236
oran2527 <orlago250183@gmail.com>
2019-10-17 19:52:53 +0000

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * string_nconcat - concatenates two pointers by n src chars
  * @s1: pointer dest
@@ -9,31 +10,30 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *pointer;
-	unsigned int i, j, k;
+	unsigned int i = 0, j = 0, k = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; *(s1 + i) != '\0'; )
+	for (i = 0; *(s1 + i) != '\0' && s1 != NULL; )
 		i++;
-	for (j = 0; *(s1 + j) != '\0'; )
+	for (j = 0; *(s2 + j) != '\0' && s2 != NULL; )
 		j++;
 	if (n >= j)
 		n = j;
+	printf("%u %u %u", i, j, n);
 	pointer = malloc((i + n + 1) * sizeof(char));
 	if (pointer != NULL)
-	{
-		for (i = 0; *(s1 + i) != '\0'; i++)
-		{
+	{	
+		for (i = 0; *(s1 + i) != '\0' && s1 != NULL && s2 != NULL; i++)
 			*(pointer + i) = *(s1 + i);
-		}
-		for (k = 0; k < n; k++)
-		{
-			*(pointer + i + k) = *(s2 + k);
-		}
-		*(pointer + k + i) = '\0';
+		for (k = 0; k < n && s1 != NULL && s2 != NULL; k++)
+			*(pointer + i + k) = *(s2 + k);		
+		for (k = 0; k != '\0' && s1 == NULL && s2 != NULL; k++)
+			*(pointer + k) = *(s2 + k);
+		*(pointer + (i + n + 1)) = '\0';
 		return (pointer);
 	}
-	return (NULL);
+	return (pointer);
 }
