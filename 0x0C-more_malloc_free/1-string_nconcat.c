24e120e32b54857e322431c6f146258d1cda0178
oran2527 <orlago250183@gmail.com>
2019-10-17 21:30:26 +0000

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,7 +9,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *pointer;
-	unsigned int i = 0, j = 0, k, m;
+	unsigned int i = 0, j = 0, k = 0 , m = 0;
 
 	if (s1 != NULL)
 		for (i = 0; *(s1 + i) != '\0'; )
