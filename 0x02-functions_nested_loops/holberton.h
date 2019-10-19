99e0768fa25bc0222da6b81691bfa8994d5765b4
oran2525 <raumedina010101@gmail.com>
2019-10-19 21:06:02 +0000

    first

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -19,5 +19,11 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_alphabet(void);
+void print_times_table(int n)
+
+void print_spaces(int amount);
+void divide_1000(int resul);
+void divide_100(int resul);
+void divide_10(int resul);
 
 #endif /* _HOLBERTON_H_ */
