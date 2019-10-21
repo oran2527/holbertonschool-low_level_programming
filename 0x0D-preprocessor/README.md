4e552b6448b308b90534d2ed21c4ad237a63ce7c
oran2527 <orlago250183@gmail.com>
2019-10-21 20:15:08 +0000

    first

--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,1 +1,1 @@
-preprocessorpreprocessor
+preprocessor
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * prototypes
+ */
+
+int _putchar(char c);
+
+/**
+ * struct dog - struct of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct of a dog wit name age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _HOLBERTON_H_ */
