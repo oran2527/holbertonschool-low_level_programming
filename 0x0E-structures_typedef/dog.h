ba9a3877a2cf0d98022eae97734bf8a44812d1a6
oran2527 <orlago250183@gmail.com>
2019-10-22 01:11:15 +0000

    first

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -29,8 +29,10 @@ struct dog
 
 typedef struct dog dog_t;
 
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+unsigned int count_str(char *ptr);
 
 #endif /* _HOLBERTON_H_ */
