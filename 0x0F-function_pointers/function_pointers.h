8628fa6b3509f10d705ec300d09b2d49b6984dec
oran2525 <raumedina010101@gmail.com>
2019-10-23 19:14:28 +0000

    first

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * prototypes
+ */
+
+
+int _putchar(char c);
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* _HOLBERTON_H_ */
