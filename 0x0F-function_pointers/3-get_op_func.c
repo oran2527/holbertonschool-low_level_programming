271b2d985d50d30db6a8c17d98403af25667e12b
oran2525 <raumedina010101@gmail.com>
2019-10-24 03:23:52 +0000

    fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -14,13 +14,11 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0, len2;
+	int i = 0;
 
-	
 	while (i < 5)
 	{
-		len2 = strlen(s);
-		if (*s == *ops[i].op && len2 == 1)
+		if (!strcmp(s, ops[i].op))
 			return (ops[i].f);
 		i++;
 	}
