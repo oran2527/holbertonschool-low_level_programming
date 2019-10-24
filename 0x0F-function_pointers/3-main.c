e7a66bd6e4a179816a20d63120b41af591c20085
oran2525 <raumedina010101@gmail.com>
2019-10-24 02:02:02 +0000

    fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,21 +12,18 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("Error\n");
-		exit(98);
+		printf("Error\n"), exit(98);
 	}
 	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
 			&& *argv[2] != '/' && *argv[2] != '%')
 	{
-		printf("Error\n");
-		exit(99);
+		printf("Error\n"), exit(99);
 	}
 	if ((*argv[2] == '/' || *argv[2] == '%') && (*argv[3] == '0'))
 	{
-		printf("Error\n");
-		exit(100);
+		printf("Error\n"), exit(100);
 	}
-	
+
 	fun = get_op_func(argv[2]);
 	ans = fun(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", ans);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -42,8 +42,7 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error");
-		exit(100);
+		printf("Error"), exit(100);
 	}
 	return (a / b);
 	
@@ -59,8 +58,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error\n");
-		exit(100);
+		printf("Error\n"), exit(100);
 	}
 	return (a % b);
 }
