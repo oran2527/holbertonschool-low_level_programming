8e2897ca54e7f17af3c086812cf60e66820bc07c
oran2527 <orlago250183@gmail.com>
2019-10-27 23:46:12 +0000

    fix

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,10 +1,14 @@
 global    main
-extern    puts
 
 section   .text
-main:                             ; This is called by the C library startup code
-mov       rdi, message            ; First integer (or pointer) argument in rdi
-call      puts                    ; puts(message)
-ret                               ; Return from main back into C library wrapper
-message:
-db        "Hello, Holberton", 0        ; Note strings must be terminated with 0 in C
+main:	  mov   rax, 1 ; system call for write
+mov       rdi, 1       ; file handle 1 is stdout
+mov       rsi, message ; address of string to output
+mov       rdx, 17	 ; number of bytes
+syscall		 ; invoke operating system to do the write
+mov       rax, 60	 ; system call for exit
+xor       rdi, rdi	 ; exit code 0
+syscall		 ; invoke operating system to exit
+
+section   .data
+message:	  db        "Hello, Holberton", 10 ; note the newline at the end
