65784b48d0eb0402f7167ca40f4e8bc8563297f6
oran2527 <orlago250183@gmail.com>
2019-10-31 17:28:15 +0000

    program count the elements in linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - print list nodes
+ * @h: node
+ * Return: number of nodes
+ **/
+size_t list_len(const list_t *h)
+{
+	unsigned int n = 0;
+	const list_t *temp;
+
+	temp = h;
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		n++;
+	}
+	n++;
+	return (n);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,5 +29,6 @@ typedef struct list_s
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* _HOLBERTON_H_ */
