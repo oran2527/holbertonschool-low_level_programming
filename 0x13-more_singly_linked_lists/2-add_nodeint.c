172f4a38d0a2827d03f3974745dcf4ec55c5fad9
oran2525 <raumedina010101@gmail.com>
2019-11-05 13:35:13 +0000

    fix errors in free memory

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,16 +8,17 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
-	listint_t *temp;
-	
+		
 	if (head == NULL)
 		return (NULL);
-	temp = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	new->n = n;
 	new->next = *head;
 	*head = new;
-	return (temp);
+	return (new);
 }
