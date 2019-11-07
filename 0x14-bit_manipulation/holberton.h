9e887d8e380007c7967a8f837ebe71f7bf77f6a3
oran2525 <raumedina010101@gmail.com>
2019-11-07 23:43:22 +0000

    program to convert binary to decimal

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * prototypes
+ */
+
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif /* _HOLBERTON_H_ */
