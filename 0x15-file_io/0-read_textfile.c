f47cce29e8e0e8fa93fbe8ce8431340a075d7340
oran2525 <raumedina010101@gmail.com>
2019-11-12 20:22:25 +0000

    fix betty errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,10 +10,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	FILE *fp;
 	size_t count = 0;
 	char c;
-	
+
 	if (filename == NULL)
 		return (0);
-	fp = fopen (filename, "r");
+	fp = fopen(filename, "r");
 	if (fp == NULL)
 		return (0);
 	if (fp != NULL)
