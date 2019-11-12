323be379c654d5aecffc76e720db0e1d245a4dee
oran2525 <raumedina010101@gmail.com>
2019-11-12 21:36:07 +0000

    fix betty errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,21 +2,21 @@
 /**
  * create_file - create a file
  * @filename: the name of the file
- * @text_context: content for the file
+ * @text_content: content for the file
  * Return: 1 if succeed if not -1
  **/
 int create_file(const char *filename, char *text_content)
 {
 	int fp;
 	size_t  i;
-	
+
 	if (filename == NULL)
 		return (-1);
 	fp = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (fp == '\0')
 		return (-1);
 	if (fp != '\0')
-	{		
+	{
 		if (text_content != NULL)
 		{
 			for (i = 0; text_content[i] != '\0'; )
