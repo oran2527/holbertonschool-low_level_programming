86be57c4759ea65593ba051f83fe440c8e9f126e
oran2525 <raumedina010101@gmail.com>
2019-11-13 00:39:32 +0000

    fix conditionals

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,28 +7,23 @@
  **/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	FILE *fp;
-	int ch, i;
+	int i, fp, check;
 
 	if (filename == NULL)
 		return (-1);
-
-
-	fp = fopen(filename, "a");
-	if (fp == NULL)
+	fp = open(filename, O_WRONLY | O_APPEND, S_IRUSR | S_IWUSR);
+	if (fp == -1)
 		return (-1);
-	ch = fgetc(fp);
-	while (ch != EOF)
-	{
-		ch = fgetc(fp);
-	}
-	if (text_content != NULL)
-	{
-		for (i = 0; text_content[i] != '\0'; )
-			i++;
-		write(0, text_content, fp);
+	if (text_content == NULL)
 		return (1);
+	for (i = 0; text_content[i] != '\0'; )
+		i++;
+	check = write(fp, text_content, i);
+	if (check == -1)
+	{
+		close(fp);
+		return (-1);
 	}
-	fclose(fp);
-	return (-1);
+	close(fp);
+	return (1);
 }
