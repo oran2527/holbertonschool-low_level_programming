06d286dc620e23aa5124510d17b51dcc89f68050
oran2525 <raumedina010101@gmail.com>
2019-11-15 14:35:52 +0000

    fix function write and read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,29 +7,41 @@
  **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
-	size_t count = 0;
-	char c;
+	int fp, amount_str = 0, count;
+	void *strings;
 
 	if (filename == NULL)
 		return (0);
-	fp = fopen(filename, "r");
-	if (fp == '\0')
+	fp = open(filename, O_RDONLY | S_IRUSR);
+	if (fp == -1)
 		return (0);
-	if (fp != '\0')
+	if (fp != -1)
 	{
-		c = getc(fp);
-		while (c != EOF && count < letters)
-		{
-			write(1, &c, 1);
-			c = getc(fp);
-			count++;
-		}
-		if (count != 0)
-			return (count);
-		if (count == 0)
+		strings = malloc(letters);
+		if (strings == NULL)
 			return (0);
+		amount_str = read(fp, strings, letters);
+		if (amount_str < 0)
+		{
+			free(strings);
+			close(fp);
+			return (0);
+		}
+		count = write(STDOUT_FILENO, strings, amount_str);
+		if (count >= 0)
+		{
+			free(strings);
+			close(fp);
+			return (count);
+		}
+		if (count < 0)
+		{
+			free(strings);
+			close(fp);
+			return (0);
+		}
 	}
-	fclose(fp);
+	free(strings);
+	close(fp);
 	return (0);
 }
