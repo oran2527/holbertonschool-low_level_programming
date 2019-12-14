3ab15da476b8e9f8848d23d1a7190941b21895fa
oran2527 <orlago250183@gmail.com>
2019-12-14 18:55:00 +0000

    point 0 print linked list betty and erase condition n null

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,8 +13,6 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (temp != NULL)
 	{
-		if (temp->n == '\0')
-			return (n)
 		if (temp->n != '\0')
 			printf("%d\n", temp->n);
 		temp = temp->next;
