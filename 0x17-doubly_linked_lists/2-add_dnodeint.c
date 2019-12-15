8838fd6ca239353aa992f3b758e937c33986baa6
oran2527 <orlago250183@gmail.com>
2019-12-15 01:13:23 +0000

    point 2 modifying logic

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,15 +11,26 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+	dlistint_t *temp;
 
-	if (head == NULL)
-		return (NULL);
+	temp = *head;
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = n;
-	new->next = *head;
-	new->prev = NULL;
-	*head = new;
+	if (temp == NULL)
+	{
+		new->prev = NULL;
+		new->n = n;
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->n = n;
+		new->next = temp;
+		new->prev = NULL;
+		*head = new;
+	}
 	return (new);
 }
