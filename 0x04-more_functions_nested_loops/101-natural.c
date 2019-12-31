b4ff03cf60f1f58ed39fc597bb722e1fe0b2889e
oran2527 <orlago250183@gmail.com>
2019-12-31 16:25:38 +0000

    point 13 betty

--- 0x04-more_functions_nested_loops/101-natural.c
+++ 0x04-more_functions_nested_loops/101-natural.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  * main - function to print sum of numbers to 1024 divisibles by 3 and 5
+ * Description: this function sums values divisibles by 3 and 5
  * Return: 0
  */
 int main(void)
 {
 	int x, sum;
 
-	for (x = 3; x < 1024; x++)
+	for (x = 0; x < 1024; x++)
 	{
 		if ((x % 3 == 0) || (x % 5 == 0))
 		{
