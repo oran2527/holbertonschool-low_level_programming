fc5e36c15842dda9ff09f5a987b0826ad57396cd
oran2527 <orlago250183@gmail.com>
2020-01-17 22:39:44 +0000

    point 0 create hash

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - function to create a hash table
+ * @size: size of the hash table
+ * Return: Pointer
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new = NULL;
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
+		return (NULL);
+
+	return (new);
+}
